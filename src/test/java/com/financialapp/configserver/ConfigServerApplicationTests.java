d25697416f3ce91fff7f109f87d87dbcdbd94ad0
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-05-10 20:51:20 +0100

    feat: init project

---
+++ src/test/java/com/financialapp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
